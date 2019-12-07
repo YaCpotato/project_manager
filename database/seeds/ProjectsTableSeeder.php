49fa3200cb0bcd70f42158b0b96c9af8d9f45786
YaCpotato <yasshisshy@gmail.com>
2019-12-08 02:08:49 +0900

    シーダー実行ファイルを追加

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -11,6 +11,19 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // テーブルのクリア
+    DB::table('projects')->truncate();
+
+    // 初期データ用意（列名をキーとする連想配列）
+    $projects = [
+              ['name' => 'Project-1'],
+              ['name' => 'Project-2'],
+              ['name' => 'Project-3']
+             ];
+
+    // 登録
+    foreach($projects as $project) {
+      \App\Project::create($project);
+    }
     }
 }
