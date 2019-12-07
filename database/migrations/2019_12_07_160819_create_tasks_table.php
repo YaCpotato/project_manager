c2244a9b10207d5265f8e4a607fb60efce0c3b77
YaCpotato <yasshisshy@gmail.com>
2019-12-08 01:33:07 +0900

    Initial commit

---
+++ database/migrations/2019_12_07_160819_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->date('created_at');
+            $table->date('completed_at');
+            $table->date('started_at');
+            $table->foreign('assignee_id')
+                    ->references('id')
+                    ->on('clients')
+                    ->onDelete('cascade')
+                    ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
