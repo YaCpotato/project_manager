b22ccfd7ad49ac6dcb63d3d08eef995382ab0865
YaCpotato <yasshisshy@gmail.com>
2020-02-03 09:20:15 +0900

    ファイル追加

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,38 @@
+@extends('project/layout')
+@section('content')
+<div class="container ops-main">
+<div class="row">
+  <div class="col-md-12">
+    <h3 class="ops-title">全タスクリスト</h3>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-9 col-md-offset-1" style="margin-right:30px;">
+    <table class="table text-center">
+      <tr>
+        <th class="text-center">ID</th>
+        <th class="text-center">プロジェクト名</th>
+        <th class="text-center">削除</th>
+      </tr>
+      @foreach($projects as $project)
+      <tr>
+        <td>
+          <a href="/project/{{ $project->id }}/edit">{{ $project->id }}</a>
+        </td>
+        <td>
+          <a href="/project/{{ $project->id }}/">{{ $project->name }}</a>
+        </td>
+        <td>
+          <form action="/project/{{ $project->id }}" method="post">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <button type="submit" class="btn btn-xs btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-trash"></span></button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </table>
+    <div><a href="/project/create" class="btn btn-default">新規作成</a></div>
+  </div>
+</div>
+@endsection
