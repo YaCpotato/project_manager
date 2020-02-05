863182e2f0f968a1adaad8d035a6b771918f1537
YaCpotato <yasshisshy@gmail.com>
2020-02-05 23:17:41 +0900

    indexOK

--- app/Http/Controllers/GoogleCalendarController.php
+++ app/Http/Controllers/GoogleCalendarController.php
@@ -16,11 +16,8 @@ class GoogleCalendarController extends Controller
 
     public function index()
     {
-        $event = new Event;
         $events = Event::get();
-        foreach($events as $event){
-            $this->GoogleCalendarService::import($event->name,$event->startDateTime,$event->endDateTime);
-        }
+        return view('calendar/index', compact('events'));
     }
 
     public function import()
